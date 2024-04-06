5e7045cc0a14545ba05d08cd75c74fd6ef4f252b
Rémi Champlon <champlonremii@gmail.com>
2024-04-06 19:33:11 +0200

    fix

--- src/app/(app)/Banner.js
+++ src/app/(app)/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
             <p className="text-sm leading-6 text-white">
                 <a href="#">
-                    <strong className="font-semibold">It's a alpha :)</strong>
+                    <strong className="font-semibold">It's an alpha :)</strong>
                 </a>
             </p>
             <div className="flex flex-1 justify-end" />

01b379b7684c808d61aad1b624fe1ba31f37a5ce
Rémi Champlon <champlonremii@gmail.com>
2024-05-05 14:37:41 +0200

    day switcher

--- src/app/(app)/tax-report/[year]/page.js
+++ src/app/(app)/tax-report/[year]/page.js
@@ -1,14 +1,46 @@
 'use client'
 
 import Header from '@/app/(app)/Header'
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/16/solid'
+import Link from 'next/link'
 
 const Page = ({ params }) => {
+    function YearSwitcher() {
+        return (
+            <div className={'flex flex-row items-center justify-between gap-6'}>
+                <Link
+                    href={'/tax-report/' + (parseInt(params.year) - 1)}
+                    className={
+                        'flex flex-row items-center hover:text-default-primary_dark transition duration-100'
+                    }>
+                    <ChevronLeftIcon className={'h-6 w-6 font-black'} />
+                    <span className={'font-bold'}>
+                        {parseInt(params.year) - 1}
+                    </span>
+                </Link>
+                <Link
+                    href={'/tax-report/' + (parseInt(params.year) + 1)}
+                    className={
+                        'flex flex-row items-center hover:text-default-primary_dark transition duration-100'
+                    }>
+                    <span className={'font-bold'}>
+                        {parseInt(params.year) + 1}
+                    </span>
+                    <ChevronRightIcon className={'h-6 w-6'} />
+                </Link>
+            </div>
+        )
+    }
+
     return (
         <>
-            <Header
-                title={'Tax report for ' + params.year}
-                className={'mb-4'}
-            />
+            <div className={'flex flex-row items-center justify-between'}>
+                <Header
+                    title={'Tax report for ' + params.year}
+                    className={'mb-4'}
+                />
+                <YearSwitcher />
+            </div>
         </>
     )
 }
